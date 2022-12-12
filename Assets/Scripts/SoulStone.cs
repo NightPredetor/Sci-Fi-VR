ce95f2c964bc69fa61663400d74458668a51745f
Nikhil Rana <38834548+NightPredetor@users.noreply.github.com>
2022-12-12 12:20:49 +0530

    Added Soul Stone features to the canvas.

--- Assets/Scripts/SoulStone.cs
+++ Assets/Scripts/SoulStone.cs
@@ -1,10 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
+/// <summary>
+/// The stone is not grabbable at the start.
+/// On touching the stone for the first time, the canvas will show up.
+/// On equipping from the canvas, the stone will become grabbable.
+/// On being dropped inside Box C, the stone will not be grabbable anymore until re-equiped.
+/// </summary>
 public class SoulStone : MonoBehaviour
 {
+    #region Serialized Fields
     [SerializeField] GameObject textObj;
+    [SerializeField] GameObject canvasObj;
+    #endregion
+
+    #region MonoBehaviour
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Return if grabbable OR the canvas is already on.
+        if (TryGetComponent(out XRGrabInteractable _) || canvasObj.activeInHierarchy)
+        {
+            return;
+        }
+
+        canvasObj.SetActive(true);
+    }
+    #endregion
 
     #region Public Methods
     public void HideText()
@@ -13,6 +36,18 @@ public class SoulStone : MonoBehaviour
         {
             textObj.SetActive(false);
         }
+    }
+
+    public void MakeGrabbable()
+    {
+        // Makes the object grabbable.
+        gameObject.AddComponent<XRGrabInteractable>();
+    }
+
+    public void RemoveGrabbable()
+    {
+        // Removes the object from being grabbable.
+        Destroy(gameObject.GetComponent<XRGrabInteractable>());
     }
     #endregion
 }
